fe2193c2f8451955113d338362d949b79e50116c
kira <ravirkr@icloud.com>
2018-12-20 15:23:29 -0600

    first commit

---
+++ src/test/java/com/ravi/springbootexample/SpringBootMavenApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ravi.springbootexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMavenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
